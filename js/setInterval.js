7be73e6b2be24e6ce78e8a3d78e173274fa31e4b
PulakBala <pulakbala0524@gmail.com>
2022-09-07 19:56:08 +0600

    first commit

---
+++ js/setInterval.js
@@ -0,0 +1,13 @@
+console.log('first');
+console.log('second');
+let seconds = 0;
+const intervalId =  setInterval(() => {
+    console.log(++seconds);
+    if(seconds == 10){
+        clearInterval(intervalId);
+    }
+}, 1000);
+console.log('fourth');
+console.log('fifth');
+
+
