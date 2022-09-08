824eb930f440069463e065383c995414fad4609c
PulakBala <pulakbala0524@gmail.com>
2022-09-08 18:11:14 +0600

    alert or confirm

---
+++ js/prompt.js
@@ -0,0 +1,18 @@
+const showAlert=()=>{
+    const num = Math.random()*20;
+    console.log(num)   
+    if(num < 5) {
+        alert(`
+            Hi i am pulak
+        `);
+    }
+}
+
+const askSomething=()=>{
+    const decision = confirm('are you coming picnic');
+    if(decision === true){
+        alert('bondhu 500 taka bikash kor')
+    } else{
+        alert('kno taka nai aile ay noile thak')
+    }
+}
